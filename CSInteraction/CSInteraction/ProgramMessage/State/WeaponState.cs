f2183c1534bc12c70951776bc33407c810895df1
map82top <36793054+map82top@users.noreply.github.com>
2019-04-16 16:46:17 +0300

    Добавил изображения

---
+++ CSInteraction/CSInteraction/ProgramMessage/State/WeaponState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSInteraction.Common;
+
+namespace CSInteraction.ProgramMessage
+{
+	[Serializable]
+	public class WeaponState : IMessage
+	{
+		public TypesWeapon TypeWeapon { get; private set; }
+		public IList<IMessage> StatesComponents { get; private set; }
+		public ulong ID { get; private set; }
+		public TypesProgramMessage TypeMessage { get; } = TypesProgramMessage.WeaponState;
+
+		public WeaponState(ulong id, TypesWeapon typeWeapon, IList<IMessage> statesComponents)
+		{
+			ID = id;
+			StatesComponents = statesComponents;
+			TypeWeapon = typeWeapon;
+		}
+	}
+}
