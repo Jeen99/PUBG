2deaaa705d866e3b1fe36782c02eee81cfe7444a
Rogachev99 <bag2dasusa54@gmail.com>
2019-05-12 22:16:19 +0300

    Fix: VectorMethod

--- BattleRoayleServer/BattleRoayleServer/Common/VectorMethod.cs
+++ BattleRoayleServer/BattleRoayleServer/Common/VectorMethod.cs
@@ -7,40 +7,52 @@ using Box2DX.Collision;
 using Box2DX.Common;
 using Box2DX.Dynamics;
 using System.Drawing;
+using Math = System.Math;
 
 namespace BattleRoayleServer
 {
-	static class VectorMethod
+	public static class VectorMethod
 	{
 		private static Random random = new Random();
+
 		public static Vec2 RotateVector(float angle, float distance)
 		{
 			//в радианах, на вход угол в градусах
 			//угол расчитывается против часовой стрелки
-			double RadAngle = angle * (System.Math.PI / 180);
+			double rad = (angle * Math.PI) / 180.0;
+
+			// для корректного расчета, иначе sin(90град) != 1.0
+			float deltaX = (float) Math.Round(Math.Cos(rad), 7);
+			float deltaY = (float) Math.Round(Math.Sin(rad), 7);
+
 			return new Vec2()
 			{
-				X = (float)(distance * System.Math.Cos(RadAngle)),
-				Y = (float)(-distance * System.Math.Sin(RadAngle))
+				X = distance * deltaX,
+				Y = distance * deltaY
 			};
 		}
 
 		public static float DefineDistance(Vec2 A, Vec2 B)
 		{
-			return (float)System.Math.Sqrt(System.Math.Pow(B.X-A.X,2) + System.Math.Pow(B.Y - A.Y, 2));
+			return (float)Math.Sqrt(Math.Pow(B.X-A.X,2) + Math.Pow(B.Y - A.Y, 2));
 		}
 
 		public static float DefineAngle(Vec2 start, Vec2 end)
 		{
 			//позиция мыши	
-			float angle = (float)(System.Math.Atan2(end.Y - start.Y, end.X - start.X) / System.Math.PI * 180);
+			float angle = (float)(Math.Atan2(end.Y - start.Y, end.X - start.X) / Math.PI * 180);
 			angle = (angle < 0) ? angle + 360 : angle;
+
 			return angle;
 		}
 
 		public static PointF CreateRandPosition(float sizeMap)
 		{
-			return new PointF(random.Next(0, (int)sizeMap), random.Next(0,(int)sizeMap));
+			return new PointF()
+			{
+				X = random.Next(0, (int)sizeMap),
+				Y = random.Next(0, (int)sizeMap)
+			};
 		}
 	}
 }
